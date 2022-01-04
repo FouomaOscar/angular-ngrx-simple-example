dbfc0c6267db4e5b730b7df5eb751cd755d38016
Oscar FOUOMA <oscarfouoma@yahoo.fr>
2022-01-04 09:18:03 -0500

    custom customer management feature initialized with ngrx feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
 import { MyCounterComponent } from './my-counter/my-counter.component';
 import { HomeComponent } from './home/home.component';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { CustomerModule } from './customer/customer.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,10 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ count: counterReducer })
+    // StoreModule.forRoot({ count: counterReducer }),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    CustomerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
